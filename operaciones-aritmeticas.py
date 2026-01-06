83a99f75c14eed4624d2718faef413ddd0cd7946
dcardenas <76147683@continental.edu.pe>
2026-01-06 18:42:28 -0500

    Actualiza operaciones aritméticas

--- operaciones-aritmeticas.py
+++ operaciones-aritmeticas.py
@@ -1,1 +1,4 @@
-print("Hello, Horld :D")
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+suma = numero1 + numero2
+print("La suma es:", suma)
