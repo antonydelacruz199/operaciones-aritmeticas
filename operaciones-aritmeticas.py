693d44224da465b6c0c2487faa7d1aea0281a1d2
dcardenas <76147683@continental.edu.pe>
2026-01-06 18:44:40 -0500

    Se implemento la suma de dos numero por DEF

--- operaciones-aritmeticas.py
+++ operaciones-aritmeticas.py
@@ -1,4 +1,7 @@
+def sumar_dos_numeros(numero1, numeo2):
+    return numero1 + numero2
+
 numero1 = int(input("Ingrese el primer número: "))
 numero2 = int(input("Ingrese el segundo número: "))
 suma = numero1 + numero2
-print("La suma es:", suma)
+print("La suma es:", sumar_dos_numeros(numero1, numero2))
